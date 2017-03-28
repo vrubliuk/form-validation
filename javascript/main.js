b54903ee0131e4c87cbda6231e1ae0f023ccd650
vrubliuk <vrubliuk@gmail.com>
2017-03-28 14:05:15 +0300

    Change inline JS for html form to external

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,18 @@
+window.onload = function () {
+  ChangeValue();
+};
+// Since placeholder-shown pseudo class doesn't work in IE and Edge, the value of each input will be changed dynamically via JS
+function ChangeValue() {
+  var x = document.querySelectorAll("#exampleInputLogin1, #exampleInputEmail1, #exampleInputPassword1, #exampleInputConfirmPassword1, #exampleInputFirstName1, #exampleInputLastName1, #exampleInputPhone1, #month1, #day1, #year1");
+  for (var i = 0; x.length; i++ ) {
+    x[i].onchange = function () {this.setAttribute('value', this.value);};
+  } 
+}
+
+
+
+
 document.getElementById("click").onclick = function () {
-  var x = document.getElementById("test").value;
+  var x = document.getElementById("exampleInputLogin1").value;
   alert (x);
-
-
-
-
+};
